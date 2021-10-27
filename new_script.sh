1e2d0137bca8f1e714f6d59c485a8ad5075c5005
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-10-27 20:46:42 -0300

    support: editing a bash script

---
+++ new_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#################################
+#   Author: Thiago
+#   Date: 2021-10-22
+#################################
+
+ls -lasrt .
+echo "\n\n >>>>>>> END OF FILES LIST\n\n"
+echo "Using REGEX"
+echo "text_2-start"
+
+echo "text_3-start"
+echo "text_4-start"
+echo "text_5-start"
+
+# Substitution with regex by using: %s/text_\(\d\)-start/texto_\1-inicio
